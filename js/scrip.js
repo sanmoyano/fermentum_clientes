0e466a2534f59fa9c4a03aa5cabf544635c60068
sanmoyano <smoyano.di@gmail.com>
2021-12-13 16:22:53 -0300

    add carrito

--- js/scrip.js
+++ js/scrip.js
@@ -24,7 +24,7 @@ fetch ('../JSON/productos.json')
                         <option value:"50">50 litros</option>
                     </select>
                 </div>
-                <button id="agregar_carrito ${producto.nombre}" class="btn btn-success">Agregar estilo</button>
+                <button id="agregar_carrito ${producto.nombre}" class="btn btn-warning">Agregar estilo</button>
             </div>`
         });        
 
@@ -38,7 +38,7 @@ fetch ('../JSON/productos.json')
         //varaible para sumar totales.
         let acumTot = 0;
 
-        //recorro el array de productos nuevamente para cargarlos en el array carrito 
+        //recorro el array de productos  para cargarlos en el array carrito y enviar al localStorage
         for (let producto of dataProductos) {
             let btnAgregar = document.getElementById(`agregar_carrito ${producto.nombre}`);
             btnAgregar.addEventListener ('click', agregarProd);
@@ -47,15 +47,46 @@ fetch ('../JSON/productos.json')
             function agregarProd () {
                 carrito.push(producto.nombre);
                 setLocal ();
-            } 
-
-            //capturo dropdown 
-            $('#select_barril').on ('change', (e) => {
-                let value = e.target.value
-                console.log(value)
-            })
-
+            }    
         }
+
+        //recorro el array para agregar el precio al carrito 
+        for (let producto of dataProductos) {
+            let btnAgregar = document.getElementById(`agregar_carrito ${producto.nombre}`);
+            btnAgregar.addEventListener ('click', agregarPrecio);
+
+            //funcion agegar precio al acumulador 
+            function agregarPrecio () {
+                acumTot += producto.precio
+                carrito.push(acumTot)
+                setLocal ()
+            }
+        }
+
+        //creo la variable para el boton carrito
+        let btnCarrito = document.getElementById('boton_carrito');
+        //consulto div carrito
+        let divCarrito = document.getElementById('canvas_carrito');
+
+        //tengo que crear una funcion que recorra el array donde se guardan los productos enviados al carrito y agregarlo al boton de "Mostrar pedido"
+        (mostrarPedido) => {
+            for (let i = 0; i < carrito.length; i += 1) {
+                divCarrito.innerHTML += `
+                <p>${carrito[i]}</p>
+                `
+            }
+        }
+
+        btnCarrito.addEventListener('click', mostrarPedido)
+
+
+
+
+        //capturo dropdown --- ver como capturar "seleccionar barril"
+        // $('#select_barril').on ('change', (e) => {
+        //     let value = e.target.value
+        //     console.log(value)
+        // })
         
     })
 
